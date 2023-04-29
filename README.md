1019202ec65f1e33126b41317df5a141be9134b6
Ajay o s <ajayosakhub@gmail.com>
2023-04-29 18:19:35 +0530

    👨‍💻CODE BY 🕊️★⃝AJAY O S©️🧚‍♂️

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # nodelog
+`@ajayos/nodelog` is a Node.js module for log messages with customizable severity levels and output formats.
+
+## Installation
+To install `@ajayos/nodelog`, run:
+```shell
+npm install @ajayos/nodelog
+```
+
+## Usage
+First, import the module into your Node.js application:
+
+```js
+const nodelog = require('@ajayos/nodelog');
+```
+
+you can log messages with the log method:
+
+```js
+log('This is an informational message', 'info');
+log('This is a warning message', 'warn');
+log('This is an error message', 'error');
+```
+You can also log messages with the default severity level by omitting the second argument:
+
+```js
+log('This is a default message');
+```
+
+for Line
+```js
+log()
+```
+
+## Message Types
+The following message types are available for the log method:
+
+* `info` &nbsp;&nbsp;(or `i`): Informational message (green text)
+* `warn` &nbsp;&nbsp;(or `w`): Warning message (yellow text)
+* `error` (or `e`): Error message (red text)
+* `debug` (or `d`): Debug message (magenta text)
+* `fatal` (or `f`): Fatal message (white text on red background)
+* `line` &nbsp;(or `l`): Horizontal line (cyan text)
+## Output Format
+The output format for each message type is as follows:
+
+* Informational message (green text): `[timestamp] [*]> message`
+* Warning message (yellow text): `[timestamp] [!]> message`
+* Error message (red text): `[timestamp] [x]> message`
+* Debug message (magenta text): `[timestamp] [*]> message`
+* Fatal message (white text on red background): `[timestamp] [!]> message`
+* Horizontal line (cyan text): `>-----------------------------<`
+## License
+nodelogger is released under the [`Apache-2.0`](/LICENSE) License.
---
+++ index.js
@@ -0,0 +1,64 @@
+const colors = require('colors');
+/**
+ * Logs the specified message with an optional message type.
+ *
+ * @param {string} text - The message to be logged.
+ * @param {string} type - The severity level of the message. Default is 'info'.
+ * Can be one of: 'error' (or 'e'), 'warn' (or 'w'), 'info' (or 'i'), 'debug' (or 'd'), 'fatal' (or 'f').
+ *
+ * The output format for different message types is as follows:
+ * - Error (or 'e'): > [x]> message (red text)
+ * - Warning (or 'w'): > [!]> message (yellow text)
+ * - Info (or 'i'): > [*]> message (green text)
+ * - Debug (or 'd'): > [*]> message (magenta text)
+ * - Fatal (or 'f'): > [!]> message (white text on red background)
+ * - line (or 'l'): >-----------------------------<
+ */
+function log(text, type = 'info') {
+    type = type.toLowerCase();
+    // default output format
+    function output() {
+        console.log(`>-----------------------------<`.cyan);
+    }
+    // if no input is provided or default output
+    if (text === undefined) return output();
+    // create a map of type codes to type names
+    const typeMap = {
+        e: 'error',
+        w: 'warn',
+        i: 'info',
+        d: 'debug',
+        f: 'fatal',
+        l: 'line'
+    };
+
+    // if the type code is recognized, set the type to its corresponding name
+    type = typeMap[type] || type;
+
+    // check the severity level and log the message with the appropriate prefix
+    switch (type) {
+        case 'error':
+            console.log(`>`.cyan + ` [`.blue + `x`.red  + `]`.blue + `>`.cyan + ` ${text}`.red);
+            break;
+        case 'warn':
+            console.log(`>`.cyan + ` [`.blue + `!`.yellow  + `]`.blue + `>`.cyan + ` ${text}`.yellow);
+            break;
+        case 'info':
+            console.log(`>`.cyan + ` [`.blue + `*`.green  + `]`.blue + `>`.cyan + ` ${text}`.green );
+            break;
+        case 'debug':
+            console.log(`>`.cyan + ` [`.blue + `*`.magenta  + `]`.blue + `>`.cyan + ` ${text}`.magenta );
+            break;
+        case 'fatal':
+            console.log(`>`.cyan + ` [`.blue + `!`.bgRed.white  + `]`.blue + `>`.cyan + ` ${text}`.bgRed.white);
+            break;
+        case 'line':
+            console.log(`>-----------------------------<`.cyan);
+            break;
+        default:
+            console.log(`>`.cyan + ` [`.blue + `*`.green  + `]`.blue + `>`.cyan + ` ${text}`.green );
+            break;
+    }
+}
+
+global.log = log;
